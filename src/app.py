8f7924663c3a301178ad61d1102ae30be1d367ed
fgg100y <1522009317@qq.com>
2024-02-14 23:44:13 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,19 +4,28 @@ import openai
 
 from audio import stt_model, tts_model
 from nlp import llm_model
+
 #  from wake_word.wake_detection import wakeBot
 from wake_word.wake_gamgin_stream import wake_gamgin
 
-init = 1
+init = 0
 goodbyes = [
     "bye",
     "再见",
 ]
+make_it_polite = False
+use_faster_whisper = True
+if use_faster_whisper:
+    faster_whisper = stt_model.load_faster_whisper()
+
 while True:
     # PART01: wake word detection
     if init and wake_gamgin():
+        model, config = tts_model.load_xtts_model()
         # responding the calling:
         tts_model.coquitts_speaker(
+            model=model,
+            config=config,
             itext="你好，金坚愿为您效劳",
             sr=24000,
             language="zh-cn",
@@ -26,20 +35,22 @@ while True:
         init = 0
 
     # PART02: speech2text (whisper-large), input and transcribe
-    # FIXME: 每次都加载一次模型，太慢了
     #  text = stt_model.transcribe(duration=3)
-    text = stt_model.transcribe_fast(duration=3)
+    #  breakpoint()
 
-    breakpoint()
+    text = stt_model.transcribe_fast(
+        model=faster_whisper, duration=6, verbose=2
+    )
 
     say_goodbye = [w for w in goodbyes if w in text]
     if len(say_goodbye) > 0:
-        tts_model.coquitts_speaker(
-            itext="回聊，再见",
-            sr=24000,
-            language="zh-cn",
-            save_wav=False,
-        )
+        if make_it_polite:
+            tts_model.coquitts_speaker(
+                itext="回聊，再见",
+                sr=24000,
+                language="zh-cn",
+                save_wav=False,
+            )
         break
 
     # PART03: query the LLMs
