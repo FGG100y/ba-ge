5ef520391a195c68a49b23a0e87e975df0060fde
fgg100y <1522009317@qq.com>
2024-02-17 10:46:01 +0800

    Upgrade to elevenlabs-v3 (0.3.0b0)
    This is the branch of v3, which means it's not the stable version.

--- src/audio/tts_elevenlabs.py
+++ src/audio/tts_elevenlabs.py
@@ -2,37 +2,66 @@
 
 - elevenlabs tts
 
+NOTE 由于依赖包处于开发状态的缘故，此模块的函数可能随时会过时:
+
+`pip install git+https://github.com/elevenlabs/elevenlabs-python.git@v3`
+
+elevenlabs==0.3.0b0
+
 """
 
-from elevenlabs import voices, generate, play
+import os
+from dotenv import load_dotenv
+#  from elevenlabs import voices, generate, play
+from elevenlabs import generate, play
+from elevenlabs.client import ElevenLabs
 
 
-def tts_11labs(intext):
+load_dotenv()
+api_key = os.getenv("elevenlabs_api_key")
+client = ElevenLabs(api_key=api_key)
+
+
+# elevenlabs 0.3.a
+def lllabs_speaker(intext):
     audio = generate(
         text=intext,
-        voice=voices()[0],
-        model="eleven_multilingual_v2",
+        voice="Alice",
+        model="eleven_turbo_v2",
     )
 
     play(audio)
 
 
 if __name__ == "__main__":
-    intext = (
-        "Hello! 你好! Hola! नमस्ते! Bonjour! こんにちは! مرحبا! 안녕하세요! Ciao!  Cześć! Привіт! வணக்கம்!",    ## noqa
-    )
-    tts_11labs(intext)
-    voices = voices()
-    labels_d = [v.labels for v in voices]
+    #  intext = "Hello! 你好! Hola! नमस्ते! Bonjour! こ Привіт!"
+    #  lllabs_speaker(intext)
+
+    all_voices = client.voices.get_all()
+    all_voices_d = all_voices.dict()['voices']
+
+    v_ids = [v.get("voice_id", "no_id") for v in all_voices_d]
+    names = [v.get("name", "no_name") for v in all_voices_d]
+    labels_d = [v.get("labels", "no_labels") for v in all_voices_d]
     infos = [
         (
-            label.get("gender", "no-gen"),
-            label.get("accent", "no-acc"),
-            label.get("use case", "MISSING"),
+            v_id,
+            name,
+            label.get("gender", "no-gender"),
+            label.get("accent", "no-accent"),
+            label.get("use case", "no-use_case"),
         )
-        for label in labels_d
+        for v_id, name, label in zip(v_ids, names, labels_d)
     ]
     print(infos)
-    #  {'british-essex', 'english-italian', 'american-southern', 'australian',
-    #  'irish', 'american', 'american-irish', 'british', 'english-swedish'}
-    #  breakpoint()
+
+    # VoiceResponse(voice_id='Xb7hH8MSUJpSbSDYk0k2', name='Alice',
+    # category='premade', labels={'accent': 'british', 'description':
+    # 'confident', 'age': 'middle aged', 'gender': 'female', 'featured': 'new',
+    # 'use case': 'news'}, available_for_tiers=[],
+    # high_quality_base_model_ids=['eleven_turbo_v2'], samples=None,
+    # fine_tuning=FineTuningResponseModel(is_allowed_to_fine_tune=True,
+    # fine_tuning_requested=None, finetuning_state=<FinetunigState.FINE_TUNED:
+    # 'fine_tuned'>, verification_failures=[], verification_attempts_count=0,
+    # manual_verification_requested=False, verification_attempts=None,
+    # slice_ids=None, manual_verification=None, language='en')
