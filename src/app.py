861a11d3f7f598569bb7390c1c98de71ab7585fe
fgg100y <1522009317@qq.com>
2024-03-02 13:43:36 +0800

    Support local LLM (with simple RAG)
    - support 7B models (GPTQ or other quantization)
    - RAG with local/private txts/pdfs, using FAISS as vector db
    - create a main() function

--- src/app.py
+++ src/app.py
@@ -1,32 +1,116 @@
+"""voice chat with "blunt" LLMs 
+
+(due to limited computation resources, say 32G RAM and RXT4050 6G)
+"""
 import openai
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from audio import stt_model, tts_hfopt_bark, tts_elevenlabs, tts_model
-from nlp import llm_model
+from nlp import llm_model, rag_llm
 from wake_word.wake_gamgin_stream import wake_gamgin
 from utils import load_yaml_cfg as yamlcfg
 
-
-init = 1
-wake_again = False
-
+# preset configurations:
 preset_shorts = yamlcfg.load_config()
 EXIT_WORDS_L = preset_shorts["tts_greetings"]["exit_words"]
 HELLOS_D = preset_shorts["tts_greetings"]["hellos"]
 GOODBYES_D = preset_shorts["tts_greetings"]["goodbyes"]
 LLM_BACKUPS_D = preset_shorts["llm_backups"]
 
+# TTS models:
 make_bot_polite = True
-use_xtts = False
-if use_xtts:
-    # use coqui-ai xtts
-    XTTS_MODEL, CONFIG = tts_model.load_xtts_model()
-else:
-    # use suno bark from huggingface transformers
-    hf_bark, bark_processor = tts_hfopt_bark.load_bark_model()
+# use coqui-ai xtts, limited to 72 chars for language='zh'
+XTTS_MODEL, CONFIG = tts_model.load_xtts_model()  # CPU only ok
+# use suno-bark from huggingface transformers, No TTS length limited
+hf_bark, bark_processor = tts_hfopt_bark.load_bark_model()
 
-
-#  use_faster_whisper = True
+# STT models:
 faster_whisper = stt_model.load_faster_whisper()
+
+# LOCAL LLMs: embedding model and chat model
+embedding_model_name = "models/hfLLMs/m3e-large"
+embedding_model = HuggingFaceEmbeddings(model_name=embedding_model_name)
+try:
+    vectordb = FAISS.load_local("./data/vectordb/faiss_index", embedding_model)
+except Exception:
+    print("No local faiss index found. Create one now.")
+    docs = rag_llm.load_and_process_document(
+        "data/pdfs/zh/novel_最后一片藤叶.pdf"
+    )
+    vectordb = rag_llm.embed_documents(docs, embedding_model)
+llm_chain = rag_llm.create_llm_chain(vectordb)
+
+
+def main(verbose=False):
+    init = 1
+    wake_again = False
+
+    while True:
+        # PART01: wake word detection
+
+        if not init and wake_again:
+            hello_again = HELLOS_D["hello_again"]
+            tts_greeting(hello_again, use_bark=False, xtts_sr=24000)
+
+        if init and wake_gamgin():
+            # responding the calling:
+            hello = HELLOS_D["say_hello"]
+            tts_greeting(hello, use_bark=False, xtts_sr=24000)
+            init = 0
+
+        # PART02: speech2text (faster-whisper-large), input and transcribe
+        speech2text = stt_model.transcribe_fast(
+            model=faster_whisper, duration=50, verbose=1
+        )
+
+        say_goodbye = [w for w in EXIT_WORDS_L if w in speech2text]
+        if len(say_goodbye) > 0:
+            if make_bot_polite:
+                intext = GOODBYES_D["say_goodbye"]
+                tts_greeting(intext, use_bark=False, xtts_sr=24000)
+
+            #  break  # 结束程序 (DEBUG only)
+
+            # 如果用户说再见，则进入等待唤醒状态
+            if wake_gamgin():
+                wake_again = True
+                continue
+
+        # PART03: query the LLMs ------------------------------------------------------
+
+        prompt = speech2text
+        #  (coqui-tts limit) response lenght < 72 chars :
+        #  prompt = speech2text + " 请简答，并且字数少于72个。"
+        # NOTE 使用 Bark-STT 部分解决了这个问题（但速度成了另一个问题）
+
+        try:  # the BIGGER LLM on server first:
+            llm_response = llm_model.run(query=prompt, verbose=True)
+            use_bark = True
+        except (ConnectionRefusedError, openai.APIConnectionError):
+            try:  # the smaller LLM locally:
+                llm_response = llm_chain.invoke(prompt)["result"]
+                if verbose:  # working as expected 2024-01-19
+                    print("usr:", prompt)
+                    print("bot:", llm_response)
+                use_bark = True
+            except Exception as e:
+                print(e)
+                llm_response = LLM_BACKUPS_D[
+                    "no_service"
+                ]  # "无法连接到大模型服务，请稍后再试"
+                use_bark = False
+
+        # PART04: TTS of LLM result
+        tts_greeting(llm_response, use_bark=use_bark, xtts_sr=24000)
+
+        #  if no_llm:
+        #      tts_greeting(llm_response, use_bark=False, xtts_sr=24000)
+        #  else:
+        #      # Bark model not work well on long text prompts,
+        #      # TODO solution: split long text into sentences:
+        #      # see https://github.com/suno-ai/bark/blob/main/notebooks/long_form_generation.ipynb
+        #      tts_greeting(llm_response, use_bark=True, xtts_sr=24000)
 
 
 def tts_greeting(
@@ -43,6 +127,7 @@ def tts_greeting(
                 model=hf_bark,
                 processor=bark_processor,
                 text_prompt=greeting,
+                voice_preset="v2/zh_speaker_1",
                 save_wav=False,
             )
         else:
@@ -64,56 +149,5 @@ def tts_greeting(
         )
 
 
-while True:
-    # PART01: wake word detection
-
-    if not init and wake_again:
-        hello_again = HELLOS_D["hello_again"]
-        tts_greeting(hello_again, xtts_sr=24000)
-
-    if init and wake_gamgin():
-        # responding the calling:
-        hello = HELLOS_D["say_hello"]
-        tts_greeting(hello, xtts_sr=24000)
-        init = 0
-
-    # PART02: speech2text (faster-whisper-large), input and transcribe
-    speech2text = stt_model.transcribe_fast(
-        model=faster_whisper, duration=50, verbose=1
-    )
-
-    say_goodbye = [w for w in EXIT_WORDS_L if w in speech2text]
-    if len(say_goodbye) > 0:
-        if make_bot_polite:
-            intext = GOODBYES_D["say_goodbye"]
-            tts_greeting(intext, xtts_sr=24000)
-
-        #  break  # 结束程序
-
-        # 如果用户说再见，则进入等待唤醒状态
-        if wake_gamgin():
-            wake_again = True
-            continue
-
-    # PART03: query the LLMs
-    try:
-        #  # limit response lenght < 72 chars (coqui-tts limit):
-        #  prompt = speech2text + " 请简答，并且字数少于72个。"
-        # NOTE 使用 Bark model 部分解决了这个问题（但速度成了另一个问题）
-        prompt = speech2text
-        llm_response = llm_model.query_llm(query=prompt, verbose=True)
-        no_llm = False
-    except (ConnectionRefusedError, openai.APIConnectionError):
-        llm_response = LLM_BACKUPS_D[
-            "no_service"
-        ]  # "无法连接到大模型服务，请稍后再试"
-        no_llm = True
-
-    # PART04: TTS of LLM result
-    if no_llm:
-        tts_greeting(llm_response, use_bark=False, xtts_sr=24000)
-    else:
-        # Bark model not work well on long text prompts,
-        # TODO solution: split long text into sentences:
-        # see https://github.com/suno-ai/bark/blob/main/notebooks/long_form_generation.ipynb
-        tts_greeting(llm_response, xtts_sr=24000)
+if __name__ == "__main__":
+    main(verbose=True)
