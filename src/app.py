b5880740c44fe261bd34b11b8c2b3255bbb6dd1b
fgg100y <1522009317@qq.com>
2024-03-07 00:57:13 +0800

    Add new module 'audio/utils/greeting.py'

--- src/app.py
+++ src/app.py
@@ -10,10 +10,11 @@ import openai
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-from audio import stt_model, tts_hfopt_bark, tts_elevenlabs, tts_model
+from audio import stt_model  # , tts_hfopt_bark, tts_elevenlabs, tts_model
 from nlp import llm_model, rag_llm
 from wake_word.wake_gamgin_stream import wake_gamgin
 from utils import load_yaml_cfg as yamlcfg
+from audio.utils.greeting import tts_greeting
 
 # preset configurations:
 preset_shorts = yamlcfg.load_config()
@@ -24,10 +25,7 @@ LLM_BACKUPS_D = preset_shorts["llm_backups"]
 
 # TTS models:
 # use coqui-ai xtts
-XTTS_MODEL, CONFIG = tts_model.load_xtts_model()  # CPU is ok
-
-# use suno-bark from hf-transformers, No length limited (but not so good for zh)
-#  hf_bark, bark_processor = tts_hfopt_bark.load_bark_model()
+#  XTTS_MODEL, CONFIG = tts_model.load_xtts_model()  # CPU is ok
 
 # STT models:
 faster_whisper = stt_model.load_faster_whisper()
@@ -74,8 +72,10 @@ def main(verbose=False):
 
         try:  # the BIGGER LLM on server first:
             llm_response = llm_model.run(query=prompt, streaming=True)
+            init_local_llm = 0  # if server is fine, do not init local llm
         except (ConnectionRefusedError, openai.APIConnectionError):
-            if init and init_local_llm:
+            #  if init and init_local_llm:
+            if init_local_llm:
                 # LOCAL LLMs: embedding model and chat model
                 embedding_model_name = "models/hfLLMs/m3e-large"
                 embedding_model = HuggingFaceEmbeddings(
@@ -92,7 +92,7 @@ def main(verbose=False):
                     )
                     vectordb = rag_llm.embed_documents(docs, embedding_model)
                 llm_chain = rag_llm.create_llm_chain(vectordb)
-                init_local_llm = 0
+                init_local_llm = 0  # init locall llm once is enough
 
             try:  # the smaller LLM locally:
                 llm_response = llm_chain.invoke(prompt)["result"]
@@ -105,47 +105,11 @@ def main(verbose=False):
                     "no_service"
                 ]  # "无法连接到大模型服务，请稍后再试"
 
-        # PART04: TTS of LLM result ---------------------------------------------------
-        try:
-            tts_greeting(llm_response, use_bark=False, xtts_sr=24000)
-        except Exception as e:
-            raise e
-
-
-def tts_greeting(
-    greeting,
-    use_bark=False,
-    use_hf_bark=True,
-    use_11labs=False,  # backup
-    use_gtts=False,  # backup
-    xtts_sr=16000,
-):
-    if use_bark:
-        if use_hf_bark:  # from transformers 4.31 onward
-            tts_hfopt_bark.speaker(
-                model=hf_bark,
-                processor=bark_processor,
-                text_prompt=greeting,
-                voice_preset="v2/zh_speaker_1",
-                save_wav=False,
-            )
-        else:
-            raise NotImplementedError  # more details see tts_suno_bark.py
-    elif use_11labs:  # only free for 10000 chars
-        tts_elevenlabs.lllabs_speaker(
-            intext=greeting, voice="Rachel", model="eleven_multilingual_v2"
-        )
-    elif use_gtts:  # FIXME gtts not work (mainly) due to network issues
-        tts_model.googletts_speaker(greeting, lang="zh-CN")
-    else:  # only 82 chars for 'zh' at a time
-        tts_model.coquitts_speaker(
-            model=XTTS_MODEL,
-            config=CONFIG,
-            intext=greeting,
-            sr=xtts_sr,
-            language="zh-cn",
-            save_wav=False,
-        )
+            # PART04: TTS of LLM result ---------------------------------------------------
+            try:
+                tts_greeting(llm_response, use_bark=False, xtts_sr=24000)
+            except Exception as e:
+                raise e
 
 
 if __name__ == "__main__":
---
+++ src/audio/utils/greeting.py
@@ -0,0 +1,44 @@
+"""TTS speaking out loud
+"""
+from audio import tts_hfopt_bark, tts_elevenlabs, tts_model
+
+XTTS_MODEL, CONFIG = tts_model.load_xtts_model()  # CPU is ok
+#  #  use suno-bark from hf-transformers, No length limited (but not so good for zh)
+#  hf_bark, bark_processor = tts_hfopt_bark.load_bark_model()
+
+
+
+def tts_greeting(
+    greeting,
+    use_bark=False,
+    use_hf_bark=True,
+    use_11labs=False,  # backup
+    use_gtts=False,  # backup
+    xtts_sr=16000,
+):
+    if use_bark:
+        if use_hf_bark:  # from transformers 4.31 onward
+            tts_hfopt_bark.speaker(
+                model=hf_bark,
+                processor=bark_processor,
+                text_prompt=greeting,
+                voice_preset="v2/zh_speaker_1",
+                save_wav=False,
+            )
+        else:
+            raise NotImplementedError  # more details see tts_suno_bark.py
+    elif use_11labs:  # only free for 10000 chars
+        tts_elevenlabs.lllabs_speaker(
+            intext=greeting, voice="Rachel", model="eleven_multilingual_v2"
+        )
+    elif use_gtts:  # FIXME gtts not work (mainly) due to network issues
+        tts_model.googletts_speaker(greeting, lang="zh-CN")
+    else:  # only 82 chars for 'zh' at a time
+        tts_model.coquitts_speaker(
+            model=XTTS_MODEL,
+            config=CONFIG,
+            intext=greeting,
+            sr=xtts_sr,
+            language="zh-cn",
+            save_wav=False,
+        )
